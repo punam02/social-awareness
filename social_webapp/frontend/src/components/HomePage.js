049ae96d01772695b661f11ae5b030bc21059280
Mesh-254 <meshack1995@gmail.com>
2024-09-08 16:22:32 +0300

    login signup working well

--- social_webapp/frontend/src/components/HomePage.js
+++ social_webapp/frontend/src/components/HomePage.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from './HeaderComponent';
-import Login from './Login';
-import Signup from './Signup';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function HomePage() {
@@ -14,10 +12,7 @@ function HomePage() {
         {/* Routes */}
         <Routes>
           {/* Define routes using 'element' */}
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          {/* Redirect from base path to /login */}
-          <Route path="/" element={<Navigate to="/login" />} />
+          
         </Routes>
       </div>
     </Router>
