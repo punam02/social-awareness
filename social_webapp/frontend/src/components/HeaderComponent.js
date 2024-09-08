dbb65cbc707f898ec872224fa1700f9e495c068d
Mesh-254 <meshack1995@gmail.com>
2024-09-08 16:20:37 +0300

    login signup working well

--- social_webapp/frontend/src/components/HeaderComponent.js
+++ social_webapp/frontend/src/components/HeaderComponent.js
@@ -1,5 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+    baseURL: "http://127.0.0.1:8000"
+});
 
 function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -9,75 +22,304 @@ function Header() {
         setMenuOpen(!menuOpen);
     };
 
-    return (
-        <nav className="bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary">
-            <div className="text-lg container mx-auto flex justify-between items-center font-medium">
-                {/* Logo Section */}
-                <div className="flex space-x-14 items-center">
+    const [currentUser, setCurrentUser] = useState();
+    const [registrationToggle, setRegistrationToggle] = useState(false);
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-                    <h2 className="text-2xl font-bold">
-                        <span className="text-black">Social</span>{" "}
-                        <span className="text-blue-500">Awareness</span>
-                    </h2>
+    useEffect(() => {
+        client.get("/campaigns/user")
+            .then(function (res) {
+                setCurrentUser(true);
+            })
+            .catch(function (error) {
+                setCurrentUser(false);
+            });
+    }, []);
+
+    function update_form_btn() {
+        if (registrationToggle) {
+            document.getElementById("form_btn").innerHTML = "Register";
+            setRegistrationToggle(false);
+        } else {
+            document.getElementById("form_btn").innerHTML = "Log in";
+            setRegistrationToggle(true);
+        }
+    }
+
+    function submitRegistration(e) {
+        e.preventDefault();
+        client.post(
+            "/campaigns/register",
+            {
+                email: email,
+                username: username,
+                password: password
+            }
+        ).then(function (res) {
+            client.post(
+                "/campaigns/login",
+                {
+                    email: email,
+                    password: password
+                }
+            ).then(function (res) {
+                setCurrentUser(true);
+            });
+        });
+    }
+
+    function submitLogin(e) {
+        e.preventDefault();
+        client.post(
+            "/campaigns/login",
+            {
+                email: email,
+                password: password
+            }
+        ).then(function (res) {
+            setCurrentUser(true);
+        });
+    }
+
+    function submitLogout(e) {
+        e.preventDefault();
+        client.post(
+            "/campaigns/logout",
+            { withCredentials: true }
+        ).then(function (res) {
+            setCurrentUser(false);
+        });
+    }
+
+    if (currentUser) {
+        return (
+            <nav className="bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary">
+                <div className="text-lg container mx-auto flex justify-between items-center font-medium">
+                    {/* Logo Section */}
+                    <div className="flex space-x-14 items-center">
+
+                        <h2 className="text-2xl font-bold">
+                            <span className="text-black">Social</span>{" "}
+                            <span className="text-blue-500">Awareness</span>
+                        </h2>
 
 
 
-                    {/* Desktop Menu */}
-                    <ul className="md:flex space-x-12 hidden">
-                        <li><a href="/" className="block text-base hover:text-gray-300">Home</a></li>
-                        <li><a href="/campaigns/home" className="block text-base hover:text-gray-300">Campaigns</a></li>
-                        <li><a href="#" className="block text-base hover:text-gray-300">Create Campaign</a></li>
-                    </ul>
-                </div>
-
-                {/* Desktop Login and Registration */}
-                <div className="space-x-12 hidden md:flex items-center text-base">
-                    <Link to="login" className="hidden lg:flex items-center hover:text-gray-300">
-                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em">
-                            <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
-                        </svg>
-                        <span className="px-1">Login</span>
-                    </Link>
-                    <Link to="signup">
-                        <button className="text-white py-2 px-5 transition-all duration-300 rounded hover:text-[#eee] bg-indigo-600">
-                            Registration
-                        </button>
-                    </Link>
-                </div>
-
-                {/* Mobile Menu Button */}
-                <div className="md:hidden">
-                    <button onClick={toggleMenu} className="text-black focus:outline-none focus:text-gray-300">
-                        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className="w-6 h-6">
-                            <path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z" />
-                        </svg>
-                    </button>
-                </div>
-            </div>
-
-            {/* Mobile Dropdown Menu */}
-            {menuOpen && (
-                <div className="bg-gray-800 text-white md:hidden shadow-xl space-y-4 z-50 p-3 rounded-md text-right fixed">
-                    <div className="flex flex-col items-center">
-                        <ul className="flex flex-col space-y-3">
-                            <li><a href="/" className="text-base hover:text-gray-300">Home</a></li>
-                            <li><a href="/campaigns" className="text-base hover:text-gray-300">Campaigns</a></li>
-                            <li><a href="/business" className="text-base hover:text-gray-300">Create Campaign</a></li>
+                        {/* Desktop Menu */}
+                        <ul className="md:flex space-x-12 hidden">
+                            <li><a href="/" className="block text-base hover:text-gray-300">Home</a></li>
+                            <li><a href="/campaigns/home" className="block text-base hover:text-gray-300">Campaigns</a></li>
+                            <li><a href="#" className="block text-base hover:text-gray-300">Create Campaign</a></li>
                         </ul>
-                        <a href="#login" className="flex items-center mt-5">
-                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" height="1em" width="1em">
-                                <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
+                    </div>
+
+                    {/* Desktop Login and Registration */}
+                    <div className="space-x-12 hidden md:flex items-center text-base">
+                        <Navbar.Text>
+                            <form onSubmit={e => submitLogout(e)}>
+                                <button
+                                    type="submit"
+                                    className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                                >
+                                    Log out
+                                </button>
+                            </form>
+                        </Navbar.Text>
+
+
+                    </div>
+
+                    {/* Mobile Menu Button */}
+                    <div className="md:hidden">
+                        <button onClick={toggleMenu} className="text-black focus:outline-none focus:text-gray-300">
+                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className="w-6 h-6">
+                                <path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z" />
                             </svg>
-                            <span className="px-1">Login</span>
-                        </a>
-                        <a href="#registration">
-                            <button className="bg-indigo-600 text-white py-2 px-5 rounded-md">Registration</button>
-                        </a>
+                        </button>
                     </div>
                 </div>
-            )}
-        </nav>
+
+                {/* Mobile Dropdown Menu */}
+                {menuOpen && (
+                    <div className="bg-gray-800 text-white md:hidden shadow-xl space-y-4 z-50 p-3 rounded-md text-right fixed">
+                        <div className="flex flex-col items-center">
+                            <ul className="flex flex-col space-y-3">
+                                <li><a href="/" className="text-base hover:text-gray-300">Home</a></li>
+                                <li><a href="/campaigns" className="text-base hover:text-gray-300">Campaigns</a></li>
+                                <li><a href="/business" className="text-base hover:text-gray-300">Create Campaign</a></li>
+                            </ul>
+                            <Navbar.Text>
+                                <form onSubmit={e => submitLogout(e)}>
+                                    <button
+                                        type="submit"
+                                        className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                                    >
+                                        Log out
+                                    </button>
+                                </form>
+                            </Navbar.Text>
+                        </div>
+                    </div>
+                )}
+            </nav>
+        );
+    }
+
+    return (
+        <div>
+
+            <nav className="bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary">
+                <div className="text-lg container mx-auto flex justify-between items-center font-medium">
+                    {/* Logo Section */}
+                    <div className="flex space-x-14 items-center">
+
+                        <h2 className="text-2xl font-bold">
+                            <span className="text-black">Social</span>{" "}
+                            <span className="text-blue-500">Awareness</span>
+                        </h2>
+
+                        {/* Desktop Menu */}
+                        <ul className="md:flex space-x-12 hidden">
+                            <li><a href="/" className="block text-base hover:text-gray-300">Home</a></li>
+                            <li><a href="/campaigns/home" className="block text-base hover:text-gray-300">Campaigns</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Desktop Login and Registration */}
+                    <div className="space-x-12 hidden md:flex items-center text-base">
+                        <Button id="form_btn" onClick={update_form_btn} variant="light">Register</Button>
+                    </div>
+
+                    {/* Mobile Menu Button */}
+                    <div className="md:hidden">
+                        <button onClick={toggleMenu} className="text-black focus:outline-none focus:text-gray-300">
+                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className="w-6 h-6">
+                                <path d="M64 384h384v-42.67H64zm0-106.67h384v-42.66H64zM64 128v42.67h384V128z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+
+                {/* Mobile Dropdown Menu */}
+                {menuOpen && (
+                    <div className="bg-gray-800 text-white md:hidden shadow-xl space-y-4 z-50 p-3 rounded-md text-right fixed">
+                        <div className="flex flex-col items-center">
+                            <ul className="flex flex-col space-y-3">
+                                <li><a href="/" className="text-base hover:text-gray-300">Home</a></li>
+                                <li><a href="/campaigns" className="text-base hover:text-gray-300">Campaigns</a></li>
+                            </ul>
+                        </div>
+                        
+                        <Button id="form_btn" onClick={update_form_btn} variant="light">Register/login</Button>
+                           
+                    
+                    </div>
+                )}
+            </nav>
+
+            {
+                registrationToggle ? (
+                    <div className="flex justify-center items-center min-h-screen">
+                        <Form onSubmit={e => submitRegistration(e)} className="w-full max-w-lg space-y-6 bg-white p-8 shadow-lg rounded-lg">
+                            {/* Email Address Field */}
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <h1 className="text-2xl font-bold text-center mb-6">Register</h1>
+                            <Form.Label className="block text-gray-700 text-sm font-bold mb-2">Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Enter email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                                <Form.Text className="text-xs text-gray-600">
+
+                                </Form.Text>
+                            </Form.Group>
+
+                            {/* Username Field */}
+                            <Form.Group className="mb-3" controlId="formBasicUsername">
+                                <Form.Label className="block text-gray-700 text-sm font-bold mb-2">Username</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Enter username"
+                                    value={username}
+                                    onChange={e => setUsername(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </Form.Group>
+
+                            {/* Password Field */}
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label className="block text-gray-700 text-sm font-bold mb-2">Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </Form.Group>
+
+                            {/* Submit Button */}
+                            <Button
+                                type="submit"
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            >
+                                Submit
+                            </Button>
+                        </Form>
+
+                    </div>
+                ) : (
+                    <div className="flex justify-center items-center min-h-screen">
+                        <Form onSubmit={e => submitLogin(e)} className="w-full max-w-lg space-y-6 bg-white p-8 shadow-lg rounded-lg">
+                        <h1 className="text-2xl font-bold text-center mb-6">Login</h1>
+
+                            {/* Email Address Field */}
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label className="block text-gray-700 text-sm font-bold mb-2">Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    placeholder="Enter email"
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                                <Form.Text className="text-xs text-gray-600">
+                                    {/* You can add a helper text here if needed */}
+                                </Form.Text>
+                            </Form.Group>
+
+                            {/* Password Field */}
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label className="block text-gray-700 text-sm font-bold mb-2">Password</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                />
+                            </Form.Group>
+
+                            {/* Submit Button */}
+                            <Button
+                                type="submit"
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            >
+                                Submit
+                            </Button>
+                        </Form>
+
+                    </div>
+                )
+            }
+        </div>
     );
+
 }
 
 export default Header;
